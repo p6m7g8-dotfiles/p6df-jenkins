8c012df1b24cc5a5f7b5f695b025db2bd384d09e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 12:10:54 -0500

    fix

--- lib/cli.sh
+++ lib/cli.sh
@@ -1,4 +1,4 @@
-
+# shellcheck shell=bash
 ######################################################################
 #<
 #
@@ -9,9 +9,9 @@
 ######################################################################
 p6df::modules::jenkins::cli::get() {
 
-    local dir=$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6git
-    mkdir -p $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-jenkins/libexec/
-    curl -o $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-jenkins/libexec/jenkins-cli.jar http://$JENKINS_HOST/jnlpJars/jenkins-cli.jar
+    local dir="$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR"/p6git
+    mkdir -p "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR"/p6df-jenkins/libexec/
+    curl -o "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR"/p6df-jenkins/libexec/jenkins-cli.jar http://"$JENKINS_HOST"/jnlpJars/jenkins-cli.jar
 
     p6_return_void
 }
